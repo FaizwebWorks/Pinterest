780e031701d3fed9af822202732d693c8d30a5c5
FaizwebWorks <webWorksbyfaiz@gmail.com>
2024-08-27 17:12:50 +0530

    login, register screens

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pinterest' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Pinterest | Register' });
 });
 
 module.exports = router;
